1ae7441c8c63a2b9d10f23ffd8cc15a99193fdda
Mohit <mohitjaiswal.bltr@gmail.com>
2025-11-16 20:02:35 +0530

    commv3

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -7,5 +7,18 @@ namespace ECommerceApp.Controllers
     [ApiController]
     public class RegistrationController : ControllerBase
     {
+        [HttpGet("/env")]
+        public IActionResult EnvTest()
+        {
+            //return Ok("Env test successful");
+            var test = Environment.GetEnvironmentVariable("testConnString") ?? "NULL";
+            //var cs = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ?? "NULL";
+
+            return Ok(new
+            {
+                test = test,
+                //connstring = cs
+            });
+        }
     }
 }
